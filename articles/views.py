b23ab75b6c3d713a51d1216bb0fa13092c297ab1
damianlapa <58368009+damianlapa@users.noreply.github.com>
2024-11-06 12:37:38 +0100

    main-view

--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Article
 
 
 def home_view(request):
-    return render(request, 'base.html')
+    articles = Article.objects.all().order_by('-publication_date')
+    return render(request, 'articles/main.html', locals())
